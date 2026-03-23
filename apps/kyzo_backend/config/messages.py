d621fc3d7de00e4889d7f8f1ad0bcefba8217478
Markus <25428460+MarkusPrivat@users.noreply.github.com>
2026-03-23 11:06:22 +0100

    Added KnowledgeMessages

--- apps/kyzo_backend/config/messages.py
+++ apps/kyzo_backend/config/messages.py
@@ -15,6 +15,15 @@ class UserMessages:
     GET_ALL_USER_ERROR = "Error fetching users:"
     GET_USER_ERROR = "Error fetching user:"
     NO_USERS = "No users in database!"
-    UPDATE_USER_ERROR = "Update error:"
+    UPDATE_USER_ERROR = "Update user error:"
     USER_NOT_FOUND = "User not found!"
 
+
+@dataclass(frozen=True)
+class KnowledgeMessages:
+    CREATE_ERROR = "Error adding subject:"
+    GET_ALL_SUBJECTS_ERROR = "Error fetching subjects:"
+    GET_SUBJECT_ERROR = "Error fetching subject:"
+    STATUS_UPDATE_ERROR = "Status update error:"
+    SUBJECT_ALREADY_EXISTS = "Subject already exist"
+    UPDATE_SUBJECT_ERROR = "Update subject error:"
