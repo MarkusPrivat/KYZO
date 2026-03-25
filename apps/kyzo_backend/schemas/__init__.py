69d9d0ebfcdf07352c3021903081a8a848acc6d3
Markus <25428460+MarkusPrivat@users.noreply.github.com>
2026-03-25 22:42:18 +0100

    update schemas __init__.py

--- apps/kyzo_backend/schemas/__init__.py
+++ apps/kyzo_backend/schemas/__init__.py
@@ -1,15 +1,24 @@
 from .user_schemas import UserCreate, UserRead, UserUpdate
-from .knowledge_schemas import (SubjectCreate,
-                                SubjectRead,
-                                SubjectStatus,
-                                SubjectUpdate,
-                                TopicCreate,
-                                TopicRead,
-                                TopicStatus,
-                                TopicUpdate)
-from .questions_schemas import (QuestionOption,
-                                QuestionExplanation,
-                                QuestionCreate,
-                                QuestionRead,
-                                QuestionStatus,
-                                QuestionUpdate)
+from .knowledge_schemas import (
+    SubjectCreate,
+    SubjectRead,
+    SubjectStatus,
+    SubjectUpdate,
+    TopicCreate,
+    TopicRead,
+    TopicStatus,
+    TopicUpdate
+)
+from .questions_schemas import (
+    QuestionOption,
+    QuestionExplanation,
+    QuestionInputRawInput,
+    QuestionInputExtractedQuestions,
+    QuestionCreate,
+    QuestionInputExtractedQuestionUpdate,
+    QuestionInputCreate,
+    QuestionInputUpdate,
+    QuestionRead,
+    QuestionStatus,
+    QuestionUpdate
+)
